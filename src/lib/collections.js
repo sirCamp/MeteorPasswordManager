3cd4f46e578cbb360de0e72e0963d3cb5dbab1ac
sirCamp <sircampydevelop@gmail.com>
2015-01-11 22:07:30 +0100

    Creazione struttura,collezioni,seprazione client server

---
+++ src/lib/collections.js
@@ -0,0 +1,4 @@
+
+Passwords = new Meteor.Collection('passwords');
+Tasks = new Meteor.Collection('tesks');
+
---
+++ src/server/publish.js
@@ -0,0 +1,4 @@
+ /*
+Meteor.publish('Tasks', function() {
+    return Tasks.find({});
+});*/
